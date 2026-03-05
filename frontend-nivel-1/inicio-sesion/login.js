b54d4540725dac0e12c8112e5037e845c9376f3d
sergio <cruz2020delac@gmail.com>
2026-03-04 19:54:24 -0500

    login.js

---
+++ frontend-nivel-1/inicio-sesion/login.js
@@ -0,0 +1,39 @@
+const Usuario_Correcto = "sergio";
+const Clave_Correcta = "1111";
+
+function iniciarSesion() {
+    let intentos = 0;
+    const MaximoIntentos = 3;
+
+    while (intentos < MaximoIntentos) {
+        
+        let user=prompt("Digite su usuario: ");
+        let pass = prompt("Digite su contraseña: ");
+
+        alert(`Intento ${intentos + 1}: Usuario digitado: "${user}"`);
+
+        if (user == Usuario_Correcto && pass == Clave_Correcta) {
+
+            alert("Ingreso exitoso. Bienvenido!");
+            intentos = 3; 
+
+        } else{
+
+            intentos++;
+            let restantes =  MaximoIntentos - intentos;
+            
+            if (restantes > 0) {
+                alert(`Datos incorrectos. Te quedan ${restantes} intentos.`);
+            }
+
+            else {
+                alert("Has agotado tus intentos. Acceso bloqueado.");
+            }
+
+        }
+
+    }
+
+}
+
+iniciarSesion();

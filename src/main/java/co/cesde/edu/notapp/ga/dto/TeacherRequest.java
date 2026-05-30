105104f0d77f1385f62b0b03548fd9360f67ba2c
sergio <cruz2020delac@gmail.com>
2026-05-29 19:27:25 -0500

    dto

---
+++ src/main/java/co/cesde/edu/notapp/ga/dto/TeacherRequest.java
@@ -0,0 +1,47 @@
+package co.cesde.edu.notapp.ga.dto;
+
+
+
+public class TeacherRequest {
+
+    private String firstName;
+    private String lastName;
+    private String code;
+    private Long userId;
+    private String documentNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+}
